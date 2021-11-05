eececf86b9dfc460b2062e28c1cb46e231fa5e7e
Jeremiah Thomas <jtreplies@gmail.com>
2021-11-06 00:23:35 +0400

    Added 1) Pedestrian destruct on collision, 2) Functional Immutable ScoreController

---
+++ Assets/CarScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScore : MonoBehaviour
+{
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            Destroy(other.gameObject);
+            score++;
+            Debug.Log("socre: " + score);
+        }
+    }
+}
---
+++ Assets/SupplementaryScripts/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour
+{
+
+    public GameObject player;        //Public variable to store a reference to the player game object
+    public CarScore playerComponent;
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    //Used to change score card's test
+    TextMesh text;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+
+        text = gameObject.GetComponent("TextMesh") as TextMesh;
+
+        playerComponent = player.GetComponent<CarScore>();
+
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //transform.position = player.transform.position + offset;
+        
+        if (playerComponent)
+        {
+            //Debug.Log("socre in scorecontroller: " + playerComponent.score);
+            text.text = "Score: " + playerComponent.score.ToString();
+        }
+
+    }
+}
