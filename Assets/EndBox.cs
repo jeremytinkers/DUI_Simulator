37c9be3b9ee3804e02db968f08dca62933f6be6f
Jeremiah Thomas <jtreplies@gmail.com>
2021-11-06 16:09:08 +0400

    Added finish point and stopping fn for Timer

---
+++ Assets/EndBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Timer").SendMessage("FinishSim");
+        print("Finish game!");
+    }
+}
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -6,7 +6,11 @@ public class Timer : MonoBehaviour
 {
     TextMesh text;
     private float startTime;
-    
+    private bool finished = false;
+    private string minutes = "";
+    private string seconds = "";
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +21,22 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - startTime;
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
+        if (!finished)
+        {
+            float t = Time.time - startTime;
+            minutes = ((int)t / 60).ToString();
+            seconds = (t % 60).ToString("f2");
+        }
 
         text.text = "Time: " + minutes + seconds;
 
+
+    }
+
+    public void FinishSim()
+    {
+        finished = true;
+        text.color = Color.yellow;
+        print("message received!");
     }
 }
