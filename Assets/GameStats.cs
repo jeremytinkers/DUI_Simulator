57955fdc2a2ce350d7e0e37d2cb0200fd47b378c
Jeremiah Thomas <jtreplies@gmail.com>
2021-11-16 19:32:16 +0400

    Added GameStats Canvas as a split from postprocessing

---
+++ Assets/GameStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStats : MonoBehaviour
+{
+
+    public Text scoreText;
+    public Text timeText;
+
+    public GameObject player;        //Public variable to store a reference to the player game object
+    public CarScore playerComponent;
+
+    private Timer timerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+        //Access and set Score Text
+        playerComponent = player.GetComponent<CarScore>();
+        scoreText.text = "Fatality Rate: " + playerComponent.score.ToString();
+
+        timerObject = GameObject.Find("Timer").GetComponent<Timer>();
+        timeText.text = "Timer: " + timerObject.minutes + timerObject.seconds;
+    }
+
+}
